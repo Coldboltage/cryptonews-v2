9f3633c0eff1b84cdce85ce21ffdf389517357ed
Coldboltage <alanreid@hotmail.co.uk>
2022-02-01 19:11:49 +0000

    added go back button

--- pages/posts/[id]/index.js
+++ pages/posts/[id]/index.js
@@ -1,9 +1,12 @@
 import PostItem from "../../../components/PostItem"
+import {useRouter} from "next/router"
 
 
 export default function Posts({data, id}) {
+  const router = useRouter()
   return <div className="m-10">
-    <h1 className="mb-10 text-2xl font-bold">Posts relating to {id}</h1>
+    <h1 className="mb-5 text-2xl font-bold">Posts relating to {id}</h1>
+    <button className="mb-5" type="text" onClick={() => router.push("/")}>Go Back</button>
     <ol className="list-disc">
       {data.articles.map((item, index) => <PostItem key={Math.random(Date.parse(item.publishedAt) * 3)} {...item}/>)}
     </ol>
